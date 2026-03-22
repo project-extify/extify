a41bf6c9e529b8e9329e381847e8e2b866da002c
Ayush Kumar <ayush438500@gmail.com>
2026-03-22 08:11:02 +0530

    One step closer

--- main.lua
+++ main.lua
@@ -1,12 +1,30 @@
 local args = {...}
 
 local command = args[1]
-local name = args[2]
+
+
+local function createNewFile()
+  io.write("Enter your extension name: ")
+  local name = io.read()
+  io.write("Enter your extension description: ")
+  local desc = io.read()
+  io.write("Enter your project version: ")
+  local version = io.read()
+
+  return {
+    name = name,
+    description = desc,
+    version = version,
+  }
+end
 
 if command == "create" then
   local file = io.open("hello.txt","a")
     if file ~= nil then
-      file:write("\nHello " .. name)
+      local data = createNewFile()
+      file:write("Name: " .. data.name)
+      file:write("\nDescription: " .. data.description)
+      file:write("\nVersion: " .. data.version)
       file:close()
     else
       print("Could not open the file")
